4dba85afcbcb669326d0bafe5fc180d79f98653b
Sebastian <sebastian.callh@gmail.com>
2018-09-21 22:20:02 +0200

    Modify backend to read PORT variable

--- backend/src/App.hs
+++ backend/src/App.hs
@@ -8,7 +8,9 @@ module App where
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Logger
 import           Network.Wai.Middleware.Cors
+import           Safe                        (readMay)
 import           Servant
+import           System.Environment          (lookupEnv)
 
 import qualified Db
 import           Model.Insult                (Insult, NewInsult)
@@ -21,13 +23,30 @@ type InsultAPI
 
 main :: IO ()
 main = do
+  port <- lookupSetting "PORT" 8081
   Db.run Db.migrate
   putStrLn $ "Running backend on port " ++ show port ++ "!"
   withStdoutLogger $ \logger -> do
     let settings = setPort port $ setLogger logger defaultSettings
     runSettings settings app
+
+
+lookupSetting :: Read a => String -> a -> IO a
+lookupSetting env def = do
+    maybeValue <- lookupEnv env
+    case maybeValue of
+        Nothing ->
+            return def
+        Just str ->
+            maybe (handleFailedRead str) return (readMay str)
   where
-    port = 8081
+    handleFailedRead str =
+        error $ mconcat
+            [ "Failed to read [["
+            , str
+            , "]] for environment variable "
+            , env
+            ]
 
 app :: Application
 app = cors (const $ Just corsPolicy) $ serve insultAPI server
