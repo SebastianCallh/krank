25ce252d4f179738e4a9803428a467745f95fb18
Sebastian <sebastian.callh@gmail.com>
2018-09-23 17:18:57 +0200

    Serve sound to Insult/Creator component

--- frontend/src/Insult/Api.purs
+++ frontend/src/Insult/Api.purs
@@ -5,21 +5,26 @@ import Prelude
 import Affjax (get, post, printResponseFormatError)
 import Affjax.RequestBody as RequestBody
 import Affjax.ResponseFormat as ResponseFormat
+import Audio.WebAudio.BaseAudioContext (decodeAudioDataAsync)
+import Audio.WebAudio.Types (AudioBuffer, AudioContext)
+import Control.Monad.Error.Class (throwError)
 import Data.Argonaut (decodeJson, encodeJson)
 import Data.Either (Either(..))
 import Effect.Aff (Aff)
 import Effect.Console (log)
+import Effect.Exception (error)
 import Halogen as H
 import Insult.Insult (Insult)
 
+insultUrl :: String
+insultUrl = "/insult"
 
-url :: String
-url = "http://localhost:8081/insult"
-
+staticUrl :: String
+staticUrl = "/"
 
 fetchInsults :: Aff (Either String (Array Insult))
 fetchInsults = do
-  res <- get ResponseFormat.json url
+  res <- get ResponseFormat.json insultUrl
   case res.body of
     Left err -> do
       pure <<< Left $ printResponseFormatError err
@@ -29,7 +34,7 @@ fetchInsults = do
 
 saveInsult :: Insult -> Aff Int
 saveInsult newInsult = do
-  res <- post ResponseFormat.json url $ RequestBody.json (encodeJson newInsult)
+  res <- post ResponseFormat.json insultUrl $ RequestBody.json (encodeJson newInsult)
   case res.body of
     Left err -> do
       H.liftEffect $ log $ "error"
@@ -37,3 +42,15 @@ saveInsult newInsult = do
     Right insult -> do
       H.liftEffect $ log $ "ok"
       pure 1
+
+type FileName = String
+
+fetchAudio :: AudioContext -> FileName -> Aff AudioBuffer
+fetchAudio ctx fileName = do
+  res <- get ResponseFormat.arrayBuffer $ staticUrl <> fileName    
+  case res.body of
+    Left err ->
+      throwError $ error $ printResponseFormatError err
+    Right body ->
+      decodeAudioDataAsync ctx body
+
