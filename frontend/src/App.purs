433f9178ebb373001365e0004272afa9c6e36bad
Sebastian <sebastian.callh@gmail.com>
2018-10-08 08:51:33 +0200

    Add hamburger menu layout

--- frontend/src/App.purs
+++ frontend/src/App.purs
@@ -8,12 +8,13 @@ import Data.Functor.Coproduct.Nested (Coproduct3)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff)
-import Effect.Console (log)
 import Halogen as H
 import Halogen.Component.ChildPath as CP
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Insult.Creator as Insult
+import Insult.Admin as Admin
+import Insult.Creator as Creator
 import Insult.Stats as Stats
 import Router (Route(..), routeFor, routes)
 import Routing.Hash (matches)
@@ -21,17 +22,22 @@ import User as User
 
 type Input = Unit
 
-
 data Query a
   = NavigateTo Route a
+  | ToggleNav a
 
+data NavStatus = Extended | Collapsed
+
+flipNavStatus :: NavStatus -> NavStatus
+flipNavStatus Extended = Collapsed
+flipNavStatus Collapsed = Extended
 
 type State =
   { currentRoute :: Route
+  , navStatus   :: NavStatus
   }
   
-
-type ChildQuery = Coproduct3 Insult.Query Stats.Query Insult.Query
+type ChildQuery = Coproduct3 Creator.Query Stats.Query Admin.Query
 type ChildSlot  = Either3 Unit Unit Unit
 
 
@@ -44,67 +50,106 @@ component =
     , receiver: const Nothing
     }
   where
-    initialState = { currentRoute: SelectSender }
-    
+    initialState =
+      { currentRoute: SelectSender
+      , navStatus: Collapsed
+      }
 
-    render :: State -> H.ParentHTML Query ChildQuery ChildSlot Aff
-    render state =
-        case state.currentRoute of
-          SelectSender->
-              HH.div_
-                [ header "Vem kränkte?"
-                , HH.slot' contentSlot unit Insult.component Nothing absurd
-                ]
+
+render :: State -> H.ParentHTML Query ChildQuery ChildSlot Aff
+render state =
+  case state.currentRoute of
+    SelectSender->
+      HH.div_
+      [ header "Vem kränkte?"
+      , content $ HH.slot' contentSlot unit Creator.component Nothing absurd
+      ]
                     
-          SelectReceiver userName ->
-            case User.fromUserName userName of
-              Left _err ->
-                HH.div_
-                [ header "404"
-                , HH.h2_ [ HH.text $ "Ingen användare vid namn " <> userName]
-                ]
+    SelectReceiver userName ->
+      case User.fromUserName userName of
+        Left _err ->
+          HH.div_
+          [ header "404"
+          , content $ HH.h2_ [ HH.text $ "Ingen användare vid namn " <> userName]
+          ]
                 
-              Right user ->
-                HH.div_
-                [ header $ (User.userName user) <> " kränkte vem?"
-                , HH.slot' contentSlot unit Insult.component (Just user) absurd
-                ]
+        Right user ->
+          HH.div_
+          [ header $ (User.userName user) <> " kränkte vem?"
+          , content $ HH.slot' contentSlot unit Creator.component (Just user) absurd
+          ]
 
-          Stats ->
-            HH.div_
-              [ header "Statistik"
-              , HH.slot' statsSlot unit Stats.component unit absurd
-              ]
+    Stats ->
+      HH.div_
+      [ header "Statistik"
+      , content $ HH.slot' statsSlot unit Stats.component unit absurd
+      ]
 
+    Admin ->
+      HH.div_
+      [ header "Admin"
+      , content $ HH.slot' adminSlot unit Admin.component unit absurd
+      ]
 
+  where
     header text =
       HH.div [ HP.class_ $ H.ClassName "header" ]
-      [ HH.h1_ [ HH.text text ]
-      , HH.a
-        [ HP.class_ $ H.ClassName "top-right"
-        , HP.href $ routeFor Stats
-        ]
-        [ HH.i [ HP.class_ $ H.ClassName "fa fa-bar-chart" ] []
+      [ HH.span_ [ HH.h1_ [  HH.text text ] ]
+      , HH.nav_
+        [
+          HH.div [ HP.id_ "menuToggle" ]
+          [ HH.input [ HP.type_ $ HP.InputCheckbox ]
+          , HH.div [HP.id_ "spanwrap" ]
+            [ HH.span_ []
+            , HH.span_ []
+            , HH.span_ []
+            ]
+          , HH.ul [ HP.id_ "menu" ]
+            [ HH.li_ [ menuLink SelectSender "menu-text" "Meny" "" ]
+            , HH.li_ [ menuLink SelectSender "active" "Nytt kränk" "fa fa-bullhorn" ]
+            , HH.li_ [ menuLink Stats "inactive" "Statistik" "fa fa-bar-chart"]
+            ]
+          ]
         ]
       , HH.hr_
       ]
+
+
+    content body =
+      HH.div [ HP.class_ $ H.ClassName "content" ] [ body ]
+      
+    headerClassName =
+      case state.navStatus of
+        Extended  -> "topnav responsive"
+        Collapsed -> "topnav"
+        
+    menuLink route activeClass text iconClass =
+      HH.a [ HP.class_ $ H.ClassName activeClass
+           , HP.href $ routeFor route
+           ]
+        [ HH.div_
+          [ HH.i [ HP.class_ $ H.ClassName iconClass ] []
+          , HH.text text
+          ]  
+        ]
       
     contentSlot = CP.cp1
     statsSlot   = CP.cp2
+    adminSlot   = CP.cp3
 
+eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void Aff
+eval (NavigateTo route next) = do
+  H.modify_ _ { currentRoute = route }        
+  pure next
 
-    eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void Aff
-    eval = case _ of
-      NavigateTo route next -> do
-        H.modify_ _ { currentRoute = route }        
-        pure next
+eval (ToggleNav next) = do
+  H.modify_ $ \s -> s { navStatus = flipNavStatus s.navStatus }
+  pure next
       
-
 routeSignal :: H.HalogenIO Query Void Aff -> Aff (Effect Unit)
-routeSignal driver = do
+routeSignal driver =
   H.liftEffect (matches routes hashChanged)
   where
     hashChanged _ newRoute = do
-      H.liftEffect $ log $ "updated " <> show newRoute
       _ <- launchAff $ driver.query <<< H.action $ NavigateTo newRoute
       pure unit
